016e6b923bae40648a1ee05955d4fa98e6c742c9
vslv <vslv.anpilov@gmail.com>
2022-07-20 00:27:48 +0300

    fix: A gamer fix

--- models/gamer.rb
+++ models/gamer.rb
@@ -1,24 +1,30 @@
 class Gamer
-  attr_reader :name, :cards_in_hands
+  attr_reader :name, :cards_in_hands, :cash_account, :cards_out_hands
 
-  def initialize(name, desk)
+  def initialize(name)
     @name = name
     @cash_account = 100
-    @cards_in_hands = desk.set_of_cards
-    @cards_out_hands = nil
+    @cards_in_hands = {}
+    @wins = 0
+    @losses = 0
   end
 
   def take_card(deck)
-    @cards_in_hands << deck.card
+    card = deck.card
+    @cards_in_hands[card.first] = card.last
   end
 
-  def to_pass
-    nil
-  end
+  def to_pass; end
 
   def open_cards
-    @cards_out_hands.concat(cards_in_hands)
     @cards_in_hands
+  end
+
+  def score
+    @cards_in_hands.val
+  end
+
+  def fold_cards
     @cards_in_hands.clear
   end
 end
