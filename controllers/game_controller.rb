e6f1cdc082b2a72dc204bc52729394130ae01fea
vslv <vslv.anpilov@gmail.com>
2022-07-20 23:48:26 +0300

    feat: Add dealer methods

--- controllers/game_controller.rb
+++ controllers/game_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GameController
   attr_reader :user_name
 
@@ -60,14 +62,44 @@ class GameController
     case move_number
     when 1
       @gamer.to_pass
-      puts 'Вы пассуете!'
-      dealer_move
+      puts 'Вы пасуете!'
     when 2
       @gamer.take_card(@deck)
       new_card = @gamer.cards_in_hands.keys.last
       puts "Новая карта: |#{new_card}|."
     when 3
+      puts 'Ваши карты:'
       @gamer.open_cards
+      puts "\n"
+      puts '~' * 15
+      puts 'Карты дилера:'
+      @dealer.open_cards
+      puts "\n"
+      puts '~' * 15
     end
+  end
+
+  def dealer_choice
+    move = dealer_move
+    case move
+    when 1
+      @dealer.to_pass
+      puts 'Дилер пасует!'
+    when 2
+      @dealer.take_card(@deck)
+    end
+  end
+
+  def dealer_move
+    score_sum = @dealer.cards_in_hands.values.flatten.sum
+    if (!check_ace(@dealer) && score_sum >= 17) || (check_ace(@dealer) && score_sum >= 18)
+      1
+    elsif (!check_ace(@dealer) && score_sum < 17) || (check_ace(@dealer) && score_sum < 18)
+      2
+    end
+  end
+
+  def check_ace(gamer)
+    gamer.cards_in_hands.keys.include?('A')
   end
 end
