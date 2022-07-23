9e6403e4feb09be69a49289b9455b549d331db06
vslv <vslv.anpilov@gmail.com>
2022-07-23 19:18:05 +0300

    refactor: Refactoring controller

--- controllers/game_controller.rb
+++ controllers/game_controller.rb
@@ -16,15 +16,15 @@ class GameController
     puts 'Перед началом игры введите ваше имя:'
     @gamer = Gamer.new(gets.strip.chomp)
     puts "#{@gamer.name}, добро пожаловать!"
-    puts
   end
 
   def start
     @deck.starter_set(@gamer)
     @deck.starter_set(@dealer)
-    puts "Раздача карт...\r"
+    puts 'Раздача карт...'
     loading
-    puts "Игра началась!\n"
+    system('clear')
+    puts 'Игра началась!'
   end
 
   def loading
@@ -35,30 +35,30 @@ class GameController
   end
 
   def show_cards
-    puts 'Ваши карты:'
+    puts "\nВаши карты:"
     @gamer.cards_in_hands.each_key do |card|
       print "|#{card}| "
     end
-    puts "\n" * 2
-    puts "Ваши очки: #{@gamer.points}"
+    puts
+    puts "\nВаши очки: #{@gamer.points}"
     puts "\nКарты дилера:"
     puts '|*| ' * @dealer.cards_in_hands.size
   end
 
   def to_bet
-    puts 'Ваша ставка 10 долларов.'
+    puts "\nВаша ставка 10 долларов."
     @gamer.cash_account -= 10
-    puts "\nСтавка дилера 10 долларов."
+    puts 'Ставка дилера 10 долларов.'
     @dealer.cash_account -= 10
   end
 
   def show_bank
-    puts "На вашем счете #{@gamer.cash_account} долларов."
+    puts "\nНа вашем счете #{@gamer.cash_account} долларов."
     puts "На счете дилера #{@dealer.cash_account} долларов."
   end
 
   def show_moves
-    puts 'Ваш ход! Введите номер действия, котрое хотите сделать:'
+    puts "\nВаш ход! Введите номер действия, котрое хотите сделать:"
     MOVES.each do |move_number, move|
       puts "#{move_number} -> #{move}"
     end
@@ -68,65 +68,69 @@ class GameController
     move_number = gets.to_i
     case move_number
     when 1
+      system('clear')
       gamer_pass
     when 2
+      system('clear')
       gamer_take_card
     when 3
+      system('clear')
       open_cards
     end
   end
 
   def gamer_pass
     @gamer.to_pass
-    puts 'Вы пасуете!'
+    puts "\nВы пасуете!"
     dealer_choice
   end
 
   def gamer_take_card
     @gamer.take_card(@deck)
     new_card = @gamer.cards_in_hands.keys.last
-    puts "Новая карта: |#{new_card}|."
+    puts "\nНовая карта: |#{new_card}|."
     dealer_choice
   end
 
   def open_cards
-    puts 'Ваши карты:'
+    puts "\nВы открываете карты!"
+    puts "\nВаши карты:"
     puts @gamer.open_cards
-    puts "\n" * 2
-    puts 'Карты дилера:'
+    puts "\nКарты дилера:"
     puts @dealer.open_cards
   end
 
   def dealer_choice
-    sleep 2
     score_sum = @dealer.cards_in_hands.values.sum
     if score_sum >= 17
       dealer_pass
     elsif score_sum < 17
       dealer_take_card
     end
+    open_cards
   end
 
   def dealer_pass
     @dealer.to_pass
-    puts 'Дилер пасует!'
-    open_cards
+    puts "\nДилер пасует!"
   end
 
   def dealer_take_card
     @dealer.take_card(@deck)
-    puts 'Дилер берет карту!'
+    puts "\nДилер берет карту!"
   end
 
   def scoring
-    puts 'Подсчёт очков.'
+    puts "\nПодсчёт очков..."
     loading
-    bust?
+    return if bust?
+
     if @gamer.points < @dealer.points
       puts 'Вы проиграли!'
       @dealer.wins += 1
       @dealer.cash_account += 20
     elsif @gamer.points > @dealer.points
+      puts 'Вы выиграли!'
       @gamer.wins += 1
       @gamer.cash_account += 20
     else
@@ -138,22 +142,27 @@ class GameController
 
   def bust?
     if @gamer.bust? && !@dealer.bust?
-      puts 'Вы проиграли! Перебор'
+      puts 'Вы проиграли! Перебор.'
       @dealer.wins += 1
       @dealer.cash_account += 20
+      true
     elsif !@gamer.bust? && @dealer.bust?
-      puts 'Вы выиграли! У дилера перебор'
+      puts 'Вы выиграли! У дилера перебор.'
       @gamer.wins += 1
       @gamer.cash_account += 20
+      true
     elsif @gamer.bust? && @dealer.bust?
-      puts 'Ничья! У обоих игроков перебор'
+      puts 'Ничья! У обоих игроков перебор.'
       @dealer.cash_account += 10
       @gamer.cash_account += 10
+      true
+    else
+      false
     end
   end
 
   def game_score
-    puts 'Количество очков по результатам партии:'
+    puts "\nКоличество очков по результатам партии:"
     puts "#{@gamer.name} - #{@gamer.points}"
     puts "#{@dealer.name} - #{@dealer.points}"
   end
@@ -165,35 +174,41 @@ class GameController
   end
 
   def new_game?
-    puts 'Чтобы начать новую партию, нажмите Enter'
-    puts 'Чтобы выйти, введите любой символ и нажмите Enter'
+    puts "\nЧтобы начать новую партию, нажмите Enter"
+    puts "\nЧтобы выйти, введите любой символ и нажмите Enter"
     gamer_input = gets.chomp
     if gamer_input == ''
       @gamer.fold_cards
       @dealer.fold_cards
       @deck.new_deck
+      system('clear')
+      true
     else
-      abort
+      false
     end
   end
 
   def new_match?
-    puts 'Чтобы начать новую игру, нажмите Enter'
-    puts 'Чтобы выйти, введите любой символ и нажмите Enter'
+    puts "\nЧтобы начать новую игру, нажмите Enter"
+    puts "\nЧтобы выйти, введите любой символ и нажмите Enter"
     gamer_input = gets.chomp
-    true if gamer_input == ''
-    abort
+    system('clear')
+    gamer_input == ''
   end
 
   def game_over?
     if @gamer.cash_account.zero?
-      puts 'Вы проиграли!'
+      puts 'Вы проиграли игру!'
       show_bank
       match_score
+      true
     elsif @dealer.cash_account.zero?
-      puts 'Вы выиграли!'
+      puts 'Вы выиграли игру!'
       show_bank
       match_score
+      true
+    else
+      false
     end
   end
 end
